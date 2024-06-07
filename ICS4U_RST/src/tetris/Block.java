483a5e9c1563600e6b3e0191c688adb753a478e8
rohan <rohan@10.78.24.212>
2024-06-07 12:49:28 -0400

    What was completed during this session:
    - Work on the Block class which handles individual squares within the
    grid pane
    - A default and overloaded constructor were implemented, with the
    default constructor taking x and y coordinates
    - The overloaded constructor additionally takes a type of block
    - Mutator methods for the x and y coordinates so the block can be moved
    along the grid
    - Accessor methods for the x and y coordinates so the current
    coordinates of the block can be seen
    - Accessor and mutator methods for the type of block (L, J, square, S,
    T, Z)
    
    Next steps:
    - Think about if a colour parameter is needed in the block class, or if
    the type is only necessary
    - Determine if the type of shape should be handled in the shape class
    instead of the block class
    - Add the equals() method to compare different blocks

--- ICS4U_RST/src/tetris/Block.java
+++ ICS4U_RST/src/tetris/Block.java
@@ -1,5 +1,102 @@
 package tetris;
 
+/**
+ * 
+ * @author: Rohan Daves and Jake Pommainville
+ * @date: June 7, 2024
+ * @course: ICS4U
+ * @program: Block.java
+ * @purpose: A class to handle colours and coordinates of individual blocks (squares) on the gridpane
+ *
+ */
 public class Block {
-	// Hello World
+	// Variable Definition
+	private int x;  // x-coordinate of block
+	private int y;  // y-coordinate of block
+	private int type;  // Colour of the block
+	
+	/**
+	 * The default constructor method to create a block
+	 * @param xCoordinate
+	 * 		An <code>int</code> containing the x-coordinate of the block
+	 * @param yCoordinate
+	 * 		An <code>int</code> containing the y-coordinate of the block
+	 */
+	public Block(int xCoordinate, int yCoordinate) {
+		x = xCoordinate;
+		y = yCoordinate;  // Assigns the private variables the variables passed through the constructor method
+	}
+	
+	/**
+	 * An overloaded constructor method to create a block
+	 * @param xCoordinate
+	 * 		An <code>int</code> containing the x-coordinate of the block
+	 * @param yCoordinate
+	 * 		An <code>int</code> containing the y-coordinate of the block
+	 * @param shapeType
+	 * 		An <code>int</code> containing the type of block. An <code>int</code> containing the type of block. 
+	 * <code>1</code> represents a line block, <code>2</code> represents an L-block, <code>3</code> represents a J-block,
+	 * <code>4</code> represents a square block, <code>5</code> represents an S-block, 
+	 * <code>6</code> represents a T-block, <code>7</code> represents a Z-block.
+	 */
+	public Block(int xCoordinate, int yCoordinate, int shapeType) {
+		x = xCoordinate;
+		y = yCoordinate;  // Assigns the private variables the variables passed through the constructor method
+		type = shapeType;  // Assigns shapeType to type, representing the type of block
+	}
+	
+	/**
+	 * A mutator method which allows the y-coordinate of the block to be changed
+	 * @param shift
+	 * 		An <code>int</code> containing the amount by which the user wants to shift the block in the y-axis
+	 */
+	public void modY(int shift) {
+		y += shift;  // Changes the block's y-coordinate
+	}
+	
+	/**
+	 * A mutator method which allows the x-coordinate of the block to be changed
+	 * @param shift
+	 * 		An <code>int</code> containing the amount by which the user wants to shift the block in the x-axis
+	 */
+	public void modX(int shift) {
+		x += shift;  // Changes the block's x-coordinate
+	}
+	
+	/**
+	 * Accessor method which allows users to get the y-coordinate of the block
+	 * @return An <code>int</code> containing the y-coordinate of the block
+	 */
+	public int getY() {
+		return y;  // Returns the y-coordinate of the block
+	}
+	
+	/**
+	 * Accessor method which allows users to get the x-coordinate of the block
+	 * @return An <code>int</code> containing the x-coordinate of the block
+	 */
+	public int getX() {
+		return x;  // Returns the x-coordinate of the block
+	}
+	
+	/**
+	 * Accessor method which allows users to get the type of the block
+	 * @return An <code>int</code> containing the type of the block
+	 */
+	public int getType() {
+		return type;  // Returns the type of the block
+	}
+	
+	/** 
+	 * Mutator method which sets types of blocks
+	 * @param type
+	 * 		An <code>int</code> containing the type of block. <code>1</code> represents a line block, 
+	 * <code>2</code> represents an L-block, <code>3</code> represents a J-block, <code>4</code> represents a square block,
+	 * <code>5</code> represents an S-block, <code>6</code> represents a T-block, <code>7</code> represents a Z-block.
+	 */
+	public void setType(int type) {
+		this.type = type;
+		
+	}
+	
 }
